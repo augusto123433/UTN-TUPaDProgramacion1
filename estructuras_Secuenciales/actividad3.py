79e689d933e77ff9870dcb0a4503dd7bf62e0db3
Augusto <e.augusto.f@gmail.com>
2025-08-28 14:26:42 -0300

    Update actividad3.py
    se agrega actividad 2

---
+++ estructuras_Secuenciales/actividad3.py
@@ -0,0 +1,12 @@
+#actividad 1 
+
+edad=int(input("diga su edad: "))
+if edad > 18:
+    print("Es mayor de edad")
+
+#actividad 2
+nota = int(input("Ingrese su nota: "))
+if nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
